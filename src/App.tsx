02c472c9381bde6c625da624df4d0bcc30fbb683
Artur Carvalho <>
2024-04-22 08:19:25 +0200

    Try with useHooks useInterval

--- src/App.tsx
+++ src/App.tsx
@@ -1,56 +1,22 @@
-import { useEffect, useRef, useState } from 'react';
-import './App.css';
-
-type IntervalFunction = () => unknown | void;
-
-/** If delay is null, do not execute */
-export function useInterval(callback: IntervalFunction, delay: number | null) {
-  const savedCallback = useRef<IntervalFunction | null>(null);
-
-  // Remember the latest callback.
-  useEffect(() => {
-    if (delay === null) return;
-    savedCallback.current = callback;
-  });
-
-  // Set up the interval.
-  useEffect(() => {
-    if (delay === null) return;
-    function tick() {
-      if (savedCallback.current !== null) {
-        savedCallback.current();
-      }
-    }
-    const id = setInterval(tick, delay);
-    return () => clearInterval(id);
-  }, [delay]);
-}
+import { useState } from "react";
+import { useInterval } from "usehooks-ts";
+import "./App.css";
 
 function App() {
-  const [charge, setCharge] = useState<number>(24);
-  const [direction, setDirection] = useState<'up' | 'down' | 'static'>('up');
+  const [charge, setCharge] = useState<number>(1000);
 
   function updateRemainingCharge() {
     setCharge((prev) => {
-      if (direction === 'up') return prev + 1;
-      if (direction === 'down') return prev + 1;
-
-      return prev;
+      if (prev <= 0) return 0;
+      return prev - 1;
     });
   }
 
   useInterval(updateRemainingCharge, 5);
 
-  const up = () => setDirection('up');
-  const down = () => setDirection('down');
-  const stat = () => setDirection('static');
-
   return (
     <div>
       <h1>{charge}</h1>
-      <button onClick={up}>up</button>
-      <button onClick={down}>down</button>
-      <button onClick={stat}>static</button>
     </div>
   );
 }
