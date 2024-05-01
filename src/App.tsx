f8a763752ce262cba53ff1335343b488869be827
Artur Carvalho <>
2024-05-01 09:30:38 +0200

    Show the issue with random

--- src/App.tsx
+++ src/App.tsx
@@ -2,31 +2,42 @@ import { useState } from "react";
 import { useInterval } from "usehooks-ts";
 import "./App.css";
 
-/**
- * - Sandboxes like codesandbox or stackblitz hide the memory leak
- * - Same issue if used on private window (firefox or chrome)
- * - Tested on mac m1 and m2
- * - useMemo doesn't fix it
- * - the issue is not because the charge is 0 and being considered null
- *
- */
+function maybeTrue(odds: number) {
+  return Math.random() < odds;
+}
 
-function App() {
-  const [charge, setCharge] = useState<number>(500);
+function App() {  
+  const [charge, setCharge] = useState<number>(0);
 
   function updateRemainingCharge() {
     setCharge((prev) => {
-      if (prev <= 0) return 0;
-      return prev - 1;
+      const sameValue = maybeTrue(0.99);
+      return sameValue ? prev : prev + 1;
     });
   }
 
   // The fast interval is ok, the browsers will throttle it if too low.
-  useInterval(updateRemainingCharge, charge === 0 ? null : 5);
+  useInterval(updateRemainingCharge, 5);
 
+  // todo: replace <br>
   return (
     <div>
       <h1>{charge}</h1>
+      <p>
+Issue: if the next value set is the same as the previous, the memory usage will increase.
+<br /><br />
+- To debug: open devtools - performance - record - take a snapshot - record again - take another snapshot
+<br /><br />
+
+ - Sandboxes like codesandbox or stackblitz seem to hide the memory leak <br />
+ - useMemo doesn't fix it<br />
+ - Same issue if used on private window (firefox or chrome)<br />
+ - Tested on mac m1 and m2<br />
+ - useMemo doesn't fix it<br />
+ - the issue is not because the charge is 0 and being considered null<br />
+ - Collecting garbage doesn't fix it (little broom icon on devtools)<br />
+ - The issue happens if you use the `useHook-ts` or `useInterval` from the Dan Abramovs or Josh Comeaus blog<br />
+      </p>
     </div>
   );
 }
