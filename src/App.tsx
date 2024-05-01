3eeca064dee289df41d23e1b9b5dde450fb8838c
Artur Carvalho <>
2024-05-01 08:57:34 +0200

    Just make it work!

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,32 @@
-import { useState, useMemo } from "react";
+import { useState } from "react";
 import { useInterval } from "usehooks-ts";
 import "./App.css";
 
+/**
+ * - Sandboxes like codesandbox or stackblitz hide the memory leak
+ * - Same issue if used on private window (firefox or chrome)
+ * - Tested on mac m1 and m2
+ * - useMemo doesn't fix it
+ * - the issue is not because the charge is 0 and being considered null
+ *
+ */
+
 function App() {
-  const [charge, setCharge] = useState<number>(1000);
+  const [charge, setCharge] = useState<number>(500);
 
   function updateRemainingCharge() {
     setCharge((prev) => {
-      if (prev <= 1) return 1;
+      if (prev <= 0) return 0;
       return prev - 1;
     });
   }
 
-  useInterval(updateRemainingCharge, 5);
-
-  const memoCharge = useMemo(() => {
-    return charge;
-  }, [charge]);
+  // The fast interval is ok, the browsers will throttle it if too low.
+  useInterval(updateRemainingCharge, charge === 0 ? null : 5);
 
   return (
     <div>
-      <h1>{memoCharge}</h1>
+      <h1>{charge}</h1>
     </div>
   );
 }
